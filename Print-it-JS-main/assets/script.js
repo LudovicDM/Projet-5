40c91a1920a4a2a1efa8fd1cc4b4b7cc6ac74640
LudovicDM <ludooo758@gmail.com>
2023-07-02 18:19:58 +0200

    js

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -17,4 +17,63 @@ const slides = [
 	}
 ]
 
+// Rattacher les éléments HTML
+//ARROW SLIDE
+const arrow = document.querySelector(".arrow");
+const left = document.querySelector(".arrowLeft");
+const right = document.querySelector(".arrowRight");
+const tagLine = document.getElementById("tagLine");
+const image = document.querySelector(".banner-img");
+//DOTS SLIDES
+const dots = document.querySelector(".dots");
+
+//initial position 
+let position = 0;
+//boucle for
+for(let i= 0; i<slides.length; i++){
+	const dot =document.createElement("div")
+	dot.classList.add("dot")
+	if(i===position){
+		dot.classList.add("dot_selected")
+	}
+	dots.appendChild(dot)
+
+}
+function dotChange(){
+	const allDots = document.querySelectorAll(".dot")
+	allDots.forEach((dot,index)=>{
+		if(position===index){
+			dot.classList.add("dot_selected")	
+		}else{
+			dot.classList.remove("dot_selected")
+		}
+	})
+	
+		console.log(allDots[position])
+}
+
+//event click
+right.addEventListener('click', slideRight)
+console.log('right ok')
+function slideRight() {
+	position++;
+	if (position == slides.length) {
+		position = 0;
+	}
+	image.src =`./assets/images/slideshow/${slides[position].image}`;
+	tagLine.innerHTML = slides[position].tagLine;
+	dotChange()
+}
+
+left.addEventListener('click', slideLeft);
+console.log('left ok');
+function slideLeft() {
+	position--;
+	if (position < 0) {
+		position = slides.length - 1;
+	}
+	image.src = `./assets/images/slideshow/${slides[position].image}`;
+	tagLine.innerHTML = slides[position].tagLine;
+	dotChange()
+}
 
